f39fd4aa321f1635fec9bcae7e4b935cf036ab6f
Stephane0713 <stephaned2205@gmail.com>
2023-01-12 17:34:14 +0100

    changed announcement list css

--- src/pages/Announcements/index.tsx
+++ src/pages/Announcements/index.tsx
@@ -16,14 +16,26 @@ const Announcements = () => {
       { title: "Chaussure 004", price: "130 €" },
       { title: "Chaussure 005", price: "60 €" },
       { title: "Chaussure 006", price: "300 €" },
-    ],
-    [
       { title: "Chaussure 007", price: "304 €" },
       { title: "Chaussure 008", price: "30 €" },
       { title: "Chaussure 009", price: "102 €" },
       { title: "Chaussure 010", price: "130 €" },
       { title: "Chaussure 011", price: "90 €" },
       { title: "Chaussure 012", price: "1300 €" },
+    ],
+    [
+      { title: "Chaussure 011", price: "30 €" },
+      { title: "Chaussure 012", price: "300 €" },
+      { title: "Chaussure 013", price: "100 €" },
+      { title: "Chaussure 014", price: "130 €" },
+      { title: "Chaussure 015", price: "60 €" },
+      { title: "Chaussure 016", price: "300 €" },
+      { title: "Chaussure 017", price: "304 €" },
+      { title: "Chaussure 018", price: "30 €" },
+      { title: "Chaussure 019", price: "102 €" },
+      { title: "Chaussure 020", price: "130 €" },
+      { title: "Chaussure 021", price: "90 €" },
+      { title: "Chaussure 022", price: "1300 €" },
     ],
   ];
   return (
